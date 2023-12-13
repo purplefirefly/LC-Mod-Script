7985a43205b1ed61dc35ac47d88553cc32490e42
purplefirefly <>
2023-12-13 17:46:08 -0600

    LC v45 - BL 2.5.0

--- DownloadBiggerLobbyMod.ps1
+++ DownloadBiggerLobbyMod.ps1
@@ -8,21 +8,27 @@ If (Test-Path -Path "BepInEx")
 
 Invoke-WebRequest https://github.com/BepInEx/BepInEx/releases/download/v5.4.22/BepInEx_x64_5.4.22.0.zip -O bebin.zip
 Invoke-WebRequest https://thunderstore.io/package/download/2018/LC_API/2.1.1/ -O lc_api.zip 
-Invoke-WebRequest https://thunderstore.io/package/download/bizzlemip/BiggerLobby/2.4.0/ -O biggerlobby.zip
+Invoke-WebRequest https://thunderstore.io/package/download/bizzlemip/BiggerLobby/2.5.0/ -O biggerlobby.zip
+Invoke-WebRequest https://thunderstore.io/c/lethal-company/p/tinyhoot/ShipLoot/ -O shiploot.zip
 
 $bebinHash = Get-FileHash bebin.zip -Algorithm "SHA256"
 $lc_apiHash = Get-FileHash lc_api.zip -Algorithm "SHA256"
 $biggerlobbyHash = Get-FileHash biggerlobby.zip -Algorithm "SHA256"
+$shiplootHash = Get-FileHash shiploot.zip -Algorithm "SHA256"
 
 $bebinHashComp="4c149960673f0a387ba7c016c837096ab3a41309d9140f88590bb507c59eda3f"
 $lc_apiHashComp="c4a6ab0bf321968524a3342757a00f04db2ad36a41e742b5d2d40cbfb7f02a8f"
-$biggerlobbyHashComp="c751d9b8ab91d922884d84e597ba6c6083459d284af7a82389752adce70cf85a"
+$biggerlobbyHashComp="84778d20e4864905a55cfd249209a6e22e1b2b672f18e027680ead5a0d86fca9"
+$shiplootHashComp="0c5bb6d1fef37318eaec72c65947b7a4102a00b484790d0ac7380c8959832c7f"
 
-If (($bebinHash.Hash -eq $bebinHashComp) -and ($lc_api.Hash -eq $lc_apiComp) -and ($biggerlobbyHash.Hash -eq $biggerlobbyHashComp))
+If (($bebinHash.Hash -eq $bebinHashComp) -and ($lc_api.Hash -eq $lc_apiComp) -and ($biggerlobbyHash.Hash -eq $biggerlobbyHashComp) -and ($shiplootHash.Hash -eq $shiplootHashComp))
 {
   Expand-Archive bebin.zip .
   Expand-Archive -Force lc_api.zip .
   Expand-Archive -Force biggerlobby.zip .
+
+  Expand-Archive -Force shiploot.zip tmp\
+  Move-Item -Path tmp\plugins\ShipLoot\ShipLoot.dll -Destination BepInEx\plugins
 
   Write-Host "Finished!"
 }
@@ -31,7 +37,7 @@ Else
   Write-Host "Error, hashes don't match."
 }
 
-Remove-Item -Recurse bebin.zip, lc_api.zip, biggerlobby.zip, DownloadBiggerLobbyMod.ps1
+Remove-Item -Recurse bebin.zip, lc_api.zip, biggerlobby.zip, shiploot.zip, tmp\, DownloadBiggerLobbyMod.ps1
 
 
 Write-Host "Press any key to continue..."
--- README.md
+++ README.md
@@ -8,8 +8,15 @@ https://thunderstore.io/c/lethal-company/p/bizzlemip/BiggerLobby/
 Run this inside your Lethal Company folder to install the Bigger Lobby Mod.
 To run a PowerShell file you must `Right Click -> Run with PowerShell` **double-click will open it in a text editor.**
 
-Downloads and extracts; (the versions current as of 12-7-2023)
+Downloads and extracts; (the versions current as of 12-9-2023)
 BepInEx_x64_v5.4.22
 LC_API 2.1.1
-BiggerLobby 2.4.0
+BiggerLobby 2.5.0
+ShipLoot 1.0.0
+
 As well verifies all of their hashes for basic security.
+
+
+
+Additional mods:
+https://thunderstore.io/c/lethal-company/p/tinyhoot/ShipLoot/
