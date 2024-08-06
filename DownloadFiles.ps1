55bb753b099cc7ae48e943a3e6930a27ee38b0e9
purplefirefly <>
2024-08-05 22:44:13 -0500

    Modular overhaul Managing mods should be easier

--- DownloadFiles.ps1
+++ DownloadFiles.ps1
@@ -1,3 +1,28 @@
+$TempDir = ".\tmp"
+New-Item -ItemType Directory -Path $TempDir -ErrorAction SilentlyContinue | Out-Null
+
+# Class object for each file download.
+class Config
+{
+  [string]$Url
+  [string]$Hash
+  [string]$SrcFilePath
+  [string]$DestFilePath
+}
+
+# Array of above objects to make iterating over them easier. Import from file.
+# Config file Format: Url, Hash, SrcFilePath, DestFilePath.
+$DownloadConfig = Get-Content -Path "DownloadConfig.txt" | ForEach-Object {
+  $fields = $_ -split ',' # Split the line by comma
+  [Config] @{
+    Url = $fields[0]
+    Hash = $fields[1]
+    SrcFilePath = $fields[2]
+    DestFilePath = $fields[3]
+  }
+  $ItemNumber++
+}
+
 function OldClean
 {
   Remove-Item -ErrorAction SilentlyContinue -ErrorVariable RemoveError -Recurse -Path CHANGELOG.md, changelog.txt, doorstop_config.ini, icon.png, manifest.json, README.md, winhttp.dll, .doorstop_version, BepInEx\
@@ -14,39 +39,68 @@ function OldClean
   }
 }
 
+# Remove old mod files.
 If (Test-Path -Path "BepInEx")
 {
   OldClean
   Write-Host "Old Version Removed"
 }
 
-Invoke-WebRequest https://github.com/BepInEx/BepInEx/releases/download/v5.4.23.2/BepInEx_win_x64_5.4.23.2.zip -O bebin.zip
-Invoke-WebRequest https://thunderstore.io/package/download/notnotnotswipez/MoreCompany/1.9.4/ -O morecompany.zip
-Invoke-WebRequest https://thunderstore.io/package/download/tinyhoot/ShipLoot/1.1.0/ -O shiploot.zip
 
-$bebinHash = Get-FileHash bebin.zip -Algorithm "SHA256"
-$morecompanyHash = Get-FileHash morecompany.zip -Algorithm "SHA256"
-$shiplootHash = Get-FileHash shiploot.zip -Algorithm "SHA256"
-
-$bebinHashComp="f752ce4e838f4c305b9da1404b6745f2cff23b8bfd494f79f0c84d0a01f59b46"
-$morecompanyHashComp="1cae988f9b3562fda930fafd7b04984253591800095a542e1396905366da5866"
-$shiplootHashComp="3a3937d03e27c467e6d538d9ae90d5cf3cb946a7740d656d7623a01058f602c3"
-
-If (($bebinHash.Hash -eq $bebinHashComp) -and ($morecompanyHash.Hash -eq $morecompanyHashComp) -and ($shiplootHash.Hash -eq $shiplootHashComp))
+# Download files and compare hashes.
+# Used i instead of foreach to make it easier to reference files in temp directory by number.
+$HashMatch = $true
+$Count = $DownloadConfig.Count
+for ($i = 0; $i -lt $Count; $i++)
 {
-  $Error.Clear()
-  try
-  {
-    Expand-Archive -Force bebin.zip .
-    Expand-Archive -Force morecompany.zip .
+  Invoke-WebRequest "$($DownloadConfig[$i].Url)" -O "$TempDir\$i.zip"
+  $NewHash = Get-FileHash -Algorithm "SHA256" -Path "$TempDir\$i.zip"
 
-    Expand-Archive -Force shiploot.zip tmp\
-    Move-Item -Path tmp\plugins\ShipLoot\ShipLoot.dll -Destination BepInEx\plugins
-  }
-  catch
+  if ($($DownloadConfig[$i].Hash) -ne $NewHash.Hash)
   {
-    Write-Error $_
-    Write-Warning "Extract not completed, mods may behave strangely!"
+    $HashMatch = $false
+    break
+  }
+}
+
+# Extract files.
+If ($HashMatch -eq $true)
+{
+  for ($i = 0; $i -lt $Count; $i++)
+  {
+    # Check to see if destination is empty.
+    # If it is, parent and destination are intended to be current directory.
+    # Otherwise find parent and set destination to include ".\"
+    If ([string]::IsNullOrEmpty($($DownloadConfig[$i].DestFilePath)))
+    {
+      $Parentfolder = "."
+      $Destination = "."
+    }
+    Else
+    {
+      $Parentfolder = Split-Path $($DownloadConfig[$i].DestFilePath) -Parent
+      $Destination = ".\$($DownloadConfig[$i].DestFilePath)"
+    }
+
+    # Create temporary directory to extract to.
+    New-Item -ItemType Directory -Path $TempDir\$i -ErrorAction SilentlyContinue | Out-Null
+    
+    # Create folder to move desired files to.
+    New-Item -ItemType Directory -Path $Parentfolder -ErrorAction SilentlyContinue | Out-Null
+
+    # Try catch block only here to avoid potential errors with creating folders that exist.
+    $Error.Clear()
+    try
+    {
+      Expand-Archive -Path "$TempDir\$i.zip" -DestinationPath "$TempDir\$i" -Force
+      Move-Item -Path "$TempDir\$i\$($DownloadConfig[$i].SrcFilePath)" -Destination "$Destination" -Force
+    }
+    catch
+    {
+      Write-Error $_
+      Write-Warning "Extract incomplete, see errors!"
+      break
+    }
   }
   if (!$Error)
   {
@@ -58,7 +112,7 @@ Else
   Write-Error "Hashes don't match!"
 }
 
-Remove-Item -Recurse bebin.zip, morecompany.zip, shiploot.zip, tmp\, DownloadFiles.ps1
+Remove-Item -Recurse -Path $TempDir, DownloadConfig.txt, DownloadFiles.ps1, run.bat
 
 
 Write-Host "Press any key to continue..."
